fe5190d78c00c818fd56f8dc2e49dabfc4d72fe8
João Lucas da Costa Fagundes <joaolucasdacf@gmail.com>
2022-09-01 02:19:28 -0300

    Dijkstra Initial Development

---
+++ Dijkstra.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <windows.h>
+#include <limits.h>
+
+#define INFINITY INT_MAX / 2
+
+typedef struct graph
+{
+  int **M;      // matrix - an vector made of vectors, first * represents the indicator 'i' and 2nd * represents indicator 'j'
+  int vertices; // number of elements represented by the graph
+  int edges;    // number of edges/connections between the vertices of the graph
+} * graph;
+
+typedef enum
+{
+  BLACK,
+  BLUE,
+  GREEN,
+  CYAN,
+  RED,
+  MAGENTA,
+  BROWN,
+  LIGHTGRAY,
+  DARKGRAY,
+  LIGHTBLUE,
+  LIGHTGREEN,
+  LIGHTCYAN,
+  LIGHTRED,
+  LIGHTMAGENTA,
+  YELLOW,
+  WHITE
+} COLORS;
+
+static int __BACKGROUND = BLACK;
+static int __FOREGROUND = LIGHTGRAY;
+
+void textcolor(int letra, int fundo)
+{
+  __FOREGROUND = letra;
+  __BACKGROUND = fundo;
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+                          letra + (__BACKGROUND << 4));
+}
+
+graph graphInitiate() // the integer n represents the size of the matrix n*n
+{
+  int i, j, n;
+  system("cls");
+  printf("Informe a dimensao da matrix NxN: ");
+  scanf("%d", &n);
+  graph mainGraph = malloc(sizeof(*mainGraph)); // allocate for a mainGraph pointer
+  mainGraph->vertices = n;
+  mainGraph->edges = 0;
+  mainGraph->M = malloc(n * sizeof(int *)); // allocate an vector,
+  for (i = 0; i < n; i++)
+    mainGraph->M[i] = malloc(n * sizeof(int *));
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      mainGraph->M[i][j] = 0;
+  return mainGraph;
+}
+
+void insertEdge(graph mainGraph)
+{
+  int i, j, n;
+  printf("Informe o primeiro vertice: ");
+  scanf("%d", &i);
+  printf("Informe o segundo vertice: ");
+  scanf("%d", &j);
+  printf("Informe o peso do arco: ");
+  scanf("%d", &n);
+  if (mainGraph->M[i][j] == 0)
+  {
+    mainGraph->M[i][j] = n;
+    mainGraph->edges++;
+  }
+}
+
+void printGraph(graph mainGraph)
+{
+  int i, j;
+  for (i = 0; i < mainGraph->vertices; i++)
+  {
+    printf("|%d|", i);
+    for (j = 0; j < mainGraph->vertices; j++)
+      printf("%d:", mainGraph->M[i][j]);
+    printf("\n");
+  }
+}
+
+void Dijkstra(graph mainGraph, int iniNode)
+{
+  int i, j, minor;
+  int isOpen[mainGraph->vertices], dist[mainGraph->vertices], pred[mainGraph->vertices];
+  iniAuxGraph(mainGraph, dist, pred, isOpen, iniNode);
+  minor = openMinor(mainGraph, isOpen, dist);
+  printf("minor = %d", minor);
+  system("pause");
+}
+
+int openMinor(graph mainGraph, int *isOpen, int *dist)
+{
+  int i, minor = 0;
+  for (i = 0; i < mainGraph->vertices; i++)
+  {
+    if (isOpen[i])
+    {
+      minor = i;
+      break;
+    }
+  }
+  for (i = minor + 1; i < mainGraph->vertices; i++)
+    if (isOpen[i] && dist[i] < dist[minor])
+      minor = i;
+  return minor;
+}
+
+void iniAuxGraph(graph mainGraph, int *d, int *p, int *isOpen, int iniNode)
+{
+  int i;
+  for (i = 0; i < mainGraph->vertices; i++)
+  {
+    isOpen[i] = 1;
+    d[i] = INFINITY;
+    p[i] = -1;
+  }
+  d[iniNode] = 0;
+}
+
+int main()
+{
+  int li = 1, tecla, choice = 0, iniNode;
+
+  graph mainGraph = NULL;
+  for (;;)
+  {
+    for (;;)
+    {
+      switch (li)
+      {
+      case 1:
+        system("cls");
+        printf("\t\tMENU\n");
+        textcolor(0, 15);
+        printf("\tInicializar Grafo\n");
+        textcolor(15, 0);
+        printf("\tEncontrar Menor Caminho\n");
+        printf("\tMostrar Matriz\n");
+        printf("\tSair\n");
+        break;
+      case 2:
+        system("cls");
+        printf("\t\tMENU\n");
+        printf("\tInicializar Grafo\n");
+        textcolor(0, 15);
+        printf("\tEncontrar Menor Caminho\n");
+        textcolor(15, 0);
+        printf("\tMostrar Matriz\n");
+        printf("\tSair\n");
+        break;
+      case 3:
+        system("cls");
+        printf("\t\tMENU\n");
+        printf("\tInicializar Grafo\n");
+        printf("\tEncontrar Menor Caminho\n");
+        textcolor(0, 15);
+        printf("\tMostrar Matriz\n");
+        textcolor(15, 0);
+        printf("\tSair\n");
+        break;
+      case 4:
+        system("cls");
+        printf("\t\tMENU\n");
+        printf("\tInicializar Grafo\n");
+        printf("\tEncontrar Menor Caminho\n");
+        printf("\tRemover Series\n");
+        textcolor(0, 15);
+        printf("\tSair\n");
+        textcolor(15, 0);
+        break;
+      }
+      tecla = 0;
+      tecla = getch();
+      if (tecla == 224)
+        tecla = getch();
+      if (tecla == 72)
+        li--;
+      else if (tecla == 80)
+        li++;
+      else if (tecla == 13)
+        break;
+      if (li > 6)
+        li = 1;
+      else if (li < 1)
+        li = 6;
+    }
+    system("cls");
+    switch (li)
+    {
+    case 1:
+      mainGraph = graphInitiate();
+      for (;;)
+      {
+        system("cls");
+        printf("Inserir arco? 1-Sim 2-Nao\n");
+        printf("Escolha: ");
+        scanf("%d", &choice);
+        if (choice == 1)
+          insertEdge(mainGraph);
+        if (choice == 2)
+          break;
+      }
+      system("pause");
+      break;
+    case 2:
+      system("cls");
+      if (mainGraph == NULL)
+      {
+        printf("Ei! Voce esta pulando etapas, primeiro inicialize o grafo.");
+        system("pause");
+        break;
+      }
+      for (;;)
+      {
+        printf("Informe o vértice inicial: ");
+        scanf("%d", &iniNode);
+        if (iniNode >= 0 && iniNode <= mainGraph->vertices)
+          break;
+        else
+        {
+          printf("\nVertice inválido, favor informar novamente.");
+          system("pause");
+        }
+      }
+      Dijkstra(mainGraph, iniNode);
+      break;
+    case 3:
+      system("cls");
+      printf("3");
+      break;
+    case 4:
+      system("cls");
+      return 0;
+      break;
+    }
+  }
+  return 0;
+}
+/*int **cost = malloc(mainGraph->vertices * sizeof(int *));
+  for (i = 0; i < mainGraph->vertices; i++)
+    for (j = 0; j < mainGraph->vertices; j++)
+      cost[i] = malloc(mainGraph->vertices * sizeof(int *));
+  for (i = 0; i < mainGraph->vertices; i++)
+    for (j = 0; j < mainGraph->vertices; j++)
+    {
+      if (mainGraph->M[i][j] == 0)
+        cost[i][j] = INFINITY;
+      else
+        cost[i][j] = mainGraph->M[i][j];
+    }
+    */
